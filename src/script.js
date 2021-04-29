635ee9e220edf4afb2a124290891070f35db4388
Becks-395 <79216897+Becks-395@users.noreply.github.com>
2021-04-29 16:50:27 +0200

    Added JS for weather icon

--- src/script.js
+++ src/script.js
@@ -21,15 +21,19 @@ let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = response.data.wind.speed;
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "306b2c7792e2f4d64e57a44d766b9a83";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
+let city = "Barcelona";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 axios.get(apiUrl).then(displayTemperature);
